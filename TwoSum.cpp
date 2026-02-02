0b6cd04248ef89d51993bde28bdbecf89210a1fd
Prachi Dhende <prachidhende586@gmail.com>
2026-02-02 15:29:55 +0530

    Add files via upload

---
+++ TwoSum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr1[] = {3,4};
+    int arr2[] = {5,6};
+
+    for(int i = 0; i < 2; i++)
+    {
+        cout<<(arr1[i] + arr2[i])<<" ";
+    }
+    return 0;
+}
